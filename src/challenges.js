3f6e1e19d0cb7026f8fa1e8792b4ad75848e9515
erikadeolima <erikadeo.lima@hotmail.com>
2022-03-15 21:31:35 -0300

    Resolvido todos, exceto 7 e 9

--- src/challenges.js
+++ src/challenges.js
@@ -2,46 +2,30 @@
 function compareTrue(valor1, valor2) {
   return valor1 && valor2;
 }
-// console.log(compareTrue(false,true));
-// console.log(compareTrue(false,false));
-// console.log(compareTrue(true,true));
 
 // Desafio 2
 function calcArea(base, heigth) {
   return (base * heigth) / 2;
 }
-// console.log(calcArea(10,50));
-// console.log(calcArea(5,2));
-// console.log(calcArea(51,1));
 
 // Desafio 3
 function splitSentence(string) {
   return string.split(' ');
 }
-// console.log(splitSentence("go Trybe"));
-// console.log(splitSentence("vamo que vamo"));
-// console.log(splitSentence("foguete"));
 
 // Desafio 4
 function concatName(string2 = []) {
   // eslint-disable-next-line prefer-rest-params
   return (string2[string2.length - 1], string2[0]);
 }
-// console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
-// console.log(concatName(['foguete', 'não', 'tem', 'ré']));
-// console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
 function footballPoints(wins, ties) {
   return ((wins * 3) + (ties * 1));
 }
-// console.log(footballPoints(14, 8));
-// console.log(footballPoints(1, 2));
-// console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount(array) {
-  // eslint-disable-next-line prefer-rest-params
   let maior = Math.max.apply(...arguments, array);
   let result = [];
   for (let d = 0; d < array.length; d += 1) {
@@ -51,23 +35,18 @@ function highestCount(array) {
   }
   return (result.length);
 }
-// console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
-// console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
-// console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
+  let distancia1= math.abs()
+
   //let distancia = [];
   //for (p=0; p < distancia; p +=1) {
   //  if ()
   //}
 }
-//console.log(catAndMouse(['cat1', 0, 0, 'mouse', 0, 'cat2']));
-//console.log(catAndMouse(['cat1', 0, 0, 0, 0, 0, 'mouse', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'cat2']));
-//console.log(catAndMouse(['cat1', 0, 0, 'mouse', 0, 0, 'cat2']));
 
 // Desafio 8
-// eslint-disable-next-line sonarjs/cognitive-complexity
 function fizzBuzz(array2) {
   let result2 = [];
   for (let n = 0; n < array2.length; n += 1) {
@@ -85,9 +64,6 @@ function fizzBuzz(array2) {
   }
   return (result2);
 }
-// console.log(fizzBuzz([2, 15, 7, 9, 45]));
-// console.log(fizzBuzz([7, 9] ));
-// console.log(fizzBuzz([9, 25]));
 
 // Desafio 9
 function encode(string3) {
@@ -98,17 +74,18 @@ function decode(string3) {
 }
 
 // Desafio 10
-function techList(tech, name) {
-  let techOrdenada = tech.sort();
-  for (let techLearn of tech) {{
-  //  techLearn['tech']='tech[o]',
-  //  techLearn['name']='name'
-  //}}
+function techList(materias, pessoa) {
+  let materiasOrdenada = materias.sort();
+  let saida = [];
+  if (materias.length === 0) {
+    return ('Vazio!');
   }
-  return(techLearn);
+
+  for (let o = 0; o < materiasOrdenada.length; o += 1) {
+    saida.push({ tech: materiasOrdenada[o], name: pessoa });
+  }
+  return (saida);
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],
-"Lucas"));
 
 module.exports = {
   calcArea,
